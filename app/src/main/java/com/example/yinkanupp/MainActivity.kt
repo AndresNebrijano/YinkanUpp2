a9684955fd4d415af62dd2c91aead7e33979f9bc
Francisco Albiar Jiménez <francisco.albiar@hotmail.com>
2022-11-29 17:24:11 +0100

    Rutas y creación SplashScreen (no funciona la ruta)

--- app/src/main/java/com/example/yinkanupp/MainActivity.kt
+++ app/src/main/java/com/example/yinkanupp/MainActivity.kt
@@ -43,9 +43,10 @@ class MainActivity : ComponentActivity() {
             ) {
                 val navigationController = rememberNavController()
                 NavHost(navController = navigationController, startDestination = "pantalla1"){
-                    composable("pantalla1"){ Formulario(navigationController) }
-                    composable("pantalla2"){ ScafforldScreen(navigationController) }
-                    composable("pantalla3"){ MostrarMenuInicial(navigationController)}
+                    composable("pantalla1"){ SplashScreen(navigationController) }
+                    composable("pantalla2"){ MostrarMenuInicial(navigationController) }
+                    composable("pantalla3"){ Formulario(navigationController) }
+                    composable("pantalla4"){ ScafforldScreen(navigationController) }
                 }
             }
             }
@@ -127,7 +128,7 @@ fun app(){
 @Composable
 fun DefaultPreview(){
     YinkanUppTheme{
-        Formulario(navController= rememberNavController())
+        SplashScreen(navController= rememberNavController())
 
     }
 }
---
+++ app/src/main/java/com/example/yinkanupp/SplashScreen.kt
@@ -0,0 +1,48 @@
+package com.example.yinkanupp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+
+    LaunchedEffect(key1 = true){
+        delay(5000)
+        navController.popBackStack()
+        navController.navigate("pantalla2")
+    }
+
+
+    Splash()
+}
+
+@Composable
+fun Splash() {
+    
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        ) {
+        Image(painter = painterResource(R.drawable.logo),
+            contentDescription = "Logo YinkanUpp",
+            Modifier.size(250.dp, 250.dp))
+
+
+    }
+}
